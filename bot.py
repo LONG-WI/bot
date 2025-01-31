47119347aa78d80c1b57d797a45706e06b0a53d6
Chhinh Long <142100215+LONG-WI@users.noreply.github.com>
2025-02-01 06:22:03 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Hello! I am your Telegram bot.')
+    await update.message.reply_text('សួរស្តីខ្ញុំឈ្មោះLONGគឺជាមនុស្សយន្ត(Bot) បច្ចុប្បន្នកំពុងអភិវឌ្ឍន៍!')
 
 app = ApplicationBuilder().token("7660065153:AAE6136d6Y0H-hyTt_taMSrIvz9_TdhihJw").build()
 
