ad9d17156c1a883a20bd12e6643172f4da522bc5
Chhinh Long <142100215+LONG-WI@users.noreply.github.com>
2025-02-01 12:30:26 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,24 @@
-import logging
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger(__name__)
+# Replace 'YOUR_TOKEN' with your bot's token
+TOKEN = '7660065153:AAE6136d6Y0H-hyTt_taMSrIvz9_TdhihJw'
 
-# Define a start command handler
 def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Hello! I am your bot.')
+    update.message.reply_text('Hello! I am your bot. Send me a message and I will echo it back.')
+
+def echo(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(update.message.text)
 
 def main() -> None:
-    # Replace 'YOUR_TOKEN' with your bot's token
-    updater = Updater("7660065153:AAE6136d6Y0H-hyTt_taMSrIvz9_TdhihJw")
+    updater = Updater(TOKEN)
 
-    # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
 
-    # Register the start command handler
     dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
-    # Start the Bot
     updater.start_polling()
-
-    # Run the bot until you send a signal to stop
     updater.idle()
 
 if __name__ == '__main__':
